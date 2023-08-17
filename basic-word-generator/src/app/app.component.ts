d25c06daee80d3170fd868b74753c966699abe6b
Shantonu Chowdhury <chowdhury.shan93@gmail.com>
2023-08-17 17:22:21 +0000

    add component logic and implement value in html

--- basic-word-generator/src/app/app.component.ts
+++ basic-word-generator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayOfWord from '../utils/words';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-word-generator';
+
+  word:string =''; 
+  limit = 10;
+  handelSlidChange(newLimit:any){
+    this.limit = newLimit;
+  };
+  gaterThen(){
+    this.word = arrayOfWord.slice(0,this.limit).join(" ");    
+  }
+  getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
 }
